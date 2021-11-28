83d8f99a60cfdad117a25b619910fa40ecec681c
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-28 11:25:16 -0400

    Remove unnecessary files and setup firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCB_ngwJo19IoIJlZaalPgCenGSegeK1A0",
+  authDomain: "clone-7d961.firebaseapp.com",
+  projectId: "clone-7d961",
+  storageBucket: "clone-7d961.appspot.com",
+  messagingSenderId: "768205257807",
+  appId: "1:768205257807:web:c0478f65f143de585c7e3a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

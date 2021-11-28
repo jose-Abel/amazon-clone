599ef7e4718a67c7e03e11e50bd7936b6f4bbfe1
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-28 12:23:41 -0400

    Adding the hero image

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img 
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61Q6BYGdNxL._SX3000_.jpg" alt=""/>
+      </div>
+    </div>
+  )
+}
+
+
+export default Home

16ccb348000cb757d241e942058a3ef1dc54c144
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-28 12:08:51 -0400

    Styled the header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
         <Header />
       {/* Home */}
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
@@ -11,11 +14,13 @@ function Header() {
 
       <div className="header__search">
         <input 
-          className="header_searchInput"
+          className="header__searchInput"
           type="text"
         />
 
-        {/* Logo */}
+        <SearchIcon 
+          className="header__searchIcon"
+        />
       </div>
 
       <div className="header__nav">
@@ -32,7 +37,12 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>          
-        </div>        
+        </div>
+        
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon/>
+          <span className="header__optionLineTwo header_basketCount"></span>
+        </div>
       </div>
     </div>
   )

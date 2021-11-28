e815924e52742c79decd350ac90496b509063857
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-28 11:39:30 -0400

    Create the Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Hello</h1>
-
-      {/* Header */}
-
+        <Header />
       {/* Home */}
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header">
+      <img 
+        className="header__logo" 
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+        alt="amazon"
+      />
+
+      <div className="header__search">
+        <input 
+          className="header_searchInput"
+          type="text"
+        />
+
+        {/* Logo */}
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign in</span>          
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>          
+        </div>        
+      </div>
+    </div>
+  )
+}
+
+export default Header

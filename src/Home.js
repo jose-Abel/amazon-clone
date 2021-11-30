ac6c94ca4f556a7e28185e00b19f21ab35cf89ca
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-29 21:12:30 -0400

    Working on the Product component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from "./Product";
 import "./Home.css";
 
 function Home() {
@@ -8,10 +9,39 @@ function Home() {
         <img 
           className="home__image"
           src="https://m.media-amazon.com/images/I/61Q6BYGdNxL._SX3000_.jpg" alt=""/>
+
+          <div className="home__row">
+            <Product 
+              title="The lean startup"
+              price={28.99}
+              image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._ACSY400.jpg"
+              rating={3}
+            />
+            <Product 
+            
+            />
+          </div>
+
+          <div className="home__row">
+            <Product 
+            
+            />
+            <Product 
+            
+            />
+            <Product 
+            
+            />
+          </div>
+          
+          <div className="home__row">
+            <Product 
+            
+            />
+          </div>          
       </div>
     </div>
   )
 }
-
 
 export default Home
